053a9ad90f4719e09128a0e3341d0d4ec5eb5210
NerdvanaNC <nickunjchopra@gmail.com>
2021-06-28 18:11:53 +0530

    settings.py changes

--- djankyminiblog/settings.py
+++ djankyminiblog/settings.py
@@ -136,6 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
